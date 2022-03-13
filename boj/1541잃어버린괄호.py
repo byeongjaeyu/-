1f2ff13a899f27fdeef26ef42af892d8bbf62aa8
byeongjae <ybj121725@gmail.com>
2022-03-14 00:15:40 +0900

    boj 잃어버린괄호

--- boj/1541잃어버린괄호.py
+++ boj/1541잃어버린괄호.py
@@ -1,12 +1,32 @@
+from numpy import Inf
+
+
 def plus(x,y):
     return x+y
 def minus(x,y):
     return x-y
 
-s = input()
-plus_split = s.split('+')
-minus_split = []
-for plus in plus_split:
-    minus_split.append(plus.split('-'))
+def calculate(sts):
+    s = sts.split('+')
+    sts_sum = 0
+    for num in s:
+        sts_sum += int(num)
+    return sts_sum
 
-print(minus_split)
+s = input()
+minus_split = s.split('-')
+plus_max = [0,0]
+for i in range(1,len(minus_split)):
+    val = calculate(minus_split[i])
+    if plus_max[0] < val:
+        plus_max = [val,i]
+
+ans=  calculate(minus_split[0])
+
+for i in range(1,len(minus_split)):
+    if i != plus_max[1]:
+        ans -= calculate(minus_split[i])
+    else:
+        ans -= plus_max[0]
+
+print(ans)
